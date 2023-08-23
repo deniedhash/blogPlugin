42d1d5a8c960e722bf6f59cc99d950343b773ad3
Animesh Sinha <35229469+deniedhash@users.noreply.github.com>
2023-08-23 21:23:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,4 +10,22 @@ async function logM() {
     head.appendChild(script)
   }
 
+async function checkIframe() {
+    const iframeCheck = document.getElementById('chatbot_live_chat_widget')
+
+    if (iframeCheck && xVal === 1298) {
+      console.log('Before Retrieval Call')
+      retrieveUUIDandUSERNS()
+    } else {
+      console.log('Waiting for Iframe to exist or Element not closed yet', errorCount)
+      if (errorCount < 250) {
+        setTimeout(checkIframe, 100)
+      }
+
+      errorCount = errorCount + 1
+
+    }
+  }
+
 logM()
+checkIframe()
